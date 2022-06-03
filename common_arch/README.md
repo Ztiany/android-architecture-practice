223aa3988b17628ebd0fdd2182ff3aec54dd0fb8
ztiany <ztiany3@gmail.com>
2022-06-03 18:12:31 +0800

    adjust module

--- common_arch/README.md
+++
@@ -1,1 +0,0 @@
-# Lib Arch Base
--- common_arch/src/main/java/com/android/base/architecture/ui/state/OnRetryActionListener.kt
+++
@@ -1,9 +0,0 @@
-package com.android.base.architecture.ui.state
-
-import com.android.base.architecture.ui.state.StateLayoutConfig.RetryableState
-
-interface OnRetryActionListener {
-
-    fun onRetry(@RetryableState state: Int)
-
-}
