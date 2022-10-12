9e4b63c1ad0e699f178679bccd23a5b7c78232c6
Alien <ztiany3@gmail.com>
2022-10-12 16:52:34 +0800

    AppNavigator: optimize comment

--- common_sdk_api/src/main/java/com/android/common/api/router/AppNavigator.kt
+++ common_sdk_api/src/main/java/com/android/common/api/router/AppNavigator.kt
@@ -1,7 +1,7 @@
 package com.android.common.api.router
 
 /**
- * This is just a marker interface. All the implementations of this interface can be registered into
+ * This is just a marker interface. Any implementation of this interface can be registered into
  * [AppRouter] to provide a router leading to a specific module for other modules.
  */
 interface AppNavigator
