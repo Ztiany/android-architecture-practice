ac5e06a1b49028fef92714437c6cc1da07bc4692
ztiany <ztiany3@gmail.com>
2024-07-24 21:06:24 +0800

    optimize UIStateHandler.kt

--- common/core/src/main/java/com/app/base/utils/CallResultEx.kt
+++ common/core/src/main/java/com/app/base/utils/CallResultEx.kt
@@ -2,6 +2,9 @@ package com.app.base.utils
 
 import androidx.lifecycle.MutableLiveData
 import com.android.base.foundation.state.StateD
+import com.android.base.foundation.state.emitData
+import com.android.base.foundation.state.emitError
+import com.android.base.foundation.state.emitLoading
 import com.android.base.foundation.state.setData
 import com.android.base.foundation.state.setError
 import com.android.base.foundation.state.setLoading
@@ -9,6 +12,7 @@ import com.android.sdk.net.coroutines.CallResult
 import com.android.sdk.net.coroutines.onError
 import com.android.sdk.net.coroutines.onSuccess
 import com.app.common.api.errorhandler.ErrorHandler
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 fun <T> CallResult<T>.noticeOnError(errorHandler: ErrorHandler) {
@@ -26,6 +30,15 @@ fun <T> CallResult<T>.intoState(state: MutableStateFlow<StateD<T>>) {
     }
 }
 
+suspend fun <T> CallResult<T>.intoSharedFlow(flow: MutableSharedFlow<StateD<T>>) {
+    onSuccess {
+        flow.emitData(it)
+    }
+    onError {
+        flow.emitError(it)
+    }
+}
+
 fun <T> CallResult<T>.intoLiveData(state: MutableLiveData<StateD<T>>) {
     onSuccess {
         state.setData(it)
@@ -35,16 +48,16 @@ fun <T> CallResult<T>.intoLiveData(state: MutableLiveData<StateD<T>>) {
     }
 }
 
-suspend fun <T> MutableLiveData<StateD<T>>.syncApiCallState(startWithLoading: Boolean = true, call: suspend () -> CallResult<T>) {
-    if (startWithLoading) {
-        setLoading()
-    }
-    call.invoke().intoLiveData(this)
+suspend fun <T> MutableSharedFlow<StateD<T>>.syncApiCallState(call: suspend () -> CallResult<T>) {
+    emitLoading()
+    call.invoke().intoSharedFlow(this)
 }
 
-suspend fun <T> MutableStateFlow<StateD<T>>.syncApiCallState(startWithLoading: Boolean = true, call: suspend () -> CallResult<T>) {
-    if (startWithLoading) {
-        setLoading()
-    }
+suspend fun <T> MutableStateFlow<StateD<T>>.syncApiCallState(call: suspend () -> CallResult<T>) {
+    setLoading()
     call.invoke().intoState(this)
+}
+
+suspend fun <T> MutableLiveData<StateD<T>>.syncApiCallState(call: suspend () -> CallResult<T>) {
+    call.invoke().intoLiveData(this)
 }
