3d149f9058ec1422ac1ea7fd70508e0d1c5c584d
Alien <ztiany3@gmail.com>
2024-12-19 20:22:30 +0800

    update gradle configuration.

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -1,12 +1,24 @@
-dependencyResolutionManagement {
+pluginManagement{
     repositories {
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
         maven { url = uri("https://jitpack.io") }
         google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
+        maven { url = uri("https://jitpack.io") }
+        google()
     }
 
     versionCatalogs {
