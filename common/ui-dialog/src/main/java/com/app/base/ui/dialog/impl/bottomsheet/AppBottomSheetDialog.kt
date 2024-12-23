24c95ad0a665989b2c17cefb5c2530d2aa8bc5f7
Alien <ztiany3@gmail.com>
2024-12-23 16:13:56 +0800

    add useful comments for AppBottomSheetDialog

--- common/ui-dialog/src/main/java/com/app/base/ui/dialog/impl/bottomsheet/AppBottomSheetDialog.kt
+++ common/ui-dialog/src/main/java/com/app/base/ui/dialog/impl/bottomsheet/AppBottomSheetDialog.kt
@@ -22,12 +22,16 @@ open class AppBottomSheetDialog(
         showCompat {
             super.show()
         }
-        //https://stackoverflow.com/questions/37104960/bottomsheetdialog-with-transparent-background
+        // A way to set transparent background for BottomSheetDialog.
+        // refers to <https://stackoverflow.com/questions/37104960/bottomsheetdialog-with-transparent-background> for more details.
         findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)?.apply {
             post {
                 background = android.graphics.drawable.ColorDrawable(Color.TRANSPARENT)
             }
         }
+        // or we can use theme to set transparent background for BottomSheetDialog. check the following links for more details.
+        // <https://github.com/material-components/material-components-android/issues/267>
+        // <https://medium.com/halcyon-mobile/implementing-googles-refreshed-modal-bottom-sheet-4e76cb5de65b>
     }
 
 }
