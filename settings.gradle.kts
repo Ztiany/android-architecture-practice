09f34b51b9154844997a8fc99bf40eb40707f96e
Alien <ztiany3@gmail.com>
2025-01-03 20:32:25 +0800

    update dependencies.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,11 +21,11 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenLocal()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
         maven { url = uri("https://jitpack.io") }
-        mavenCentral()
         google()
     }
 }
