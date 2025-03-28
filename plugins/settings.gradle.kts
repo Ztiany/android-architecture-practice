d20b7b24e7b2d5cf0bf1ec5b1f60924e47ee536e
ztiany <ztiany3@gmail.com>
2025-03-28 21:08:24 +0800

    remove unused repo

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -4,7 +4,6 @@ pluginManagement{
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
-        maven { url = uri("https://jitpack.io") }
         mavenCentral()
         google()
     }
@@ -16,7 +15,6 @@ dependencyResolutionManagement {
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
-        maven { url = uri("https://jitpack.io") }
         mavenCentral()
         google()
     }
